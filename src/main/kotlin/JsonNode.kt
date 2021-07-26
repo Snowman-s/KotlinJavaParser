b2b242325fd95ca4a121e52599ed6cc8ade86b80
Snowman-s <snowman.snowsnow@gmail.com>
2021-07-26 16:22:11 +0900

    JsonNode の定義・テスト追加

---
+++ src/main/kotlin/JsonNode.kt
@@ -0,0 +1,85 @@
+interface JsonNode {
+    fun isArray(): Boolean
+    fun asArray(): Array<JsonNode>
+
+    fun isNumber(): Boolean
+    fun isInt(): Boolean
+    fun asInt(): Int
+    fun isFraction(): Boolean
+    fun asDouble(): Double
+
+    fun isBoolean(): Boolean
+    fun asBoolean(): Boolean
+
+    fun isText(): Boolean
+    fun asText(): String
+
+    fun isNull(): Boolean
+
+    fun get(key: String): JsonNode
+    fun find(key: String): JsonNode
+
+    override fun toString(): String
+
+    object MissingNode: JsonNode{
+        override fun isArray(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun asArray(): Array<JsonNode> {
+            TODO("Not yet implemented")
+        }
+
+        override fun isNumber(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun isInt(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun asInt(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun isFraction(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun asDouble(): Double {
+            TODO("Not yet implemented")
+        }
+
+        override fun isBoolean(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun asBoolean(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun isText(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun asText(): String {
+            TODO("Not yet implemented")
+        }
+
+        override fun isNull(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun get(key: String): JsonNode {
+            TODO("Not yet implemented")
+        }
+
+        override fun find(key: String): JsonNode {
+            TODO("Not yet implemented")
+        }
+
+        override fun toString(): String {
+            TODO("Not yet implemented")
+        }
+    }
+}
