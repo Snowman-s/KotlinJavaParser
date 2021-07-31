1cc154d4a278ee803c36756174330f70331e7aba
Snowman-s <snowman.snowsnow@gmail.com>
2021-08-01 01:48:29 +0900

    JSON Characterをやはり削除

--- src/main/kotlin/snowesamosc/kotlinjsonparser/factory/JsonLiteralParser.kt
+++ src/main/kotlin/snowesamosc/kotlinjsonparser/factory/JsonLiteralParser.kt
@@ -1,21 +1,5 @@
 package snowesamosc.kotlinjsonparser.factory
 
 class JsonLiteralParser {
-
-    enum class JsonCharacter(key: Char) {
-        BeginArray('\u005B'),
-        BeginObject('\u007B'),
-        EndArray('\u005D'),
-        EndObject('\u007D'),
-        NameSeparator('\u003A'),
-        ValueSeparator('\u002C'),
-        DecimalPoint('\u002E'),
-        Minus('\u002D'),
-        Plus('\u002B'),
-        Zero('\u0030'),
-        Escape('\u005C'),
-        QuotationMark('\u0022');
-
-        val value: Char = key
-    }
+    
 }
