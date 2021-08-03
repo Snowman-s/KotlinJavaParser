f64a24d1e0d2a81348bdeaa948a7d437cd03df4b
Snowman-s <snowman.snowsnow@gmail.com>
2021-08-03 19:50:46 +0900

    文字列を一部分に含むJSONリテラルのためにリテラルを追加

--- src/main/kotlin/snowesamosc/kotlinjsonparser/factory/JsonLiteralImpl.kt
+++ src/main/kotlin/snowesamosc/kotlinjsonparser/factory/JsonLiteralImpl.kt
@@ -3,11 +3,18 @@ package snowesamosc.kotlinjsonparser.factory
 import snowesamosc.kotlinjsonparser.node.JsonNode
 
 internal sealed class JsonLiteralImpl(
-    private val originalString: String,
-    private val children: List<JsonLiteral>
+    private val children: List<JsonLiteral>,
+    /**
+     * 子が存在しない際に文字列表現で使用するテキスト
+     */
+    private val originalString: String? = null
 ) : JsonLiteral {
 
-    override fun asString(): String = originalString
+    override fun asString(): String {
+        return if (hasChildren()) {
+            children.joinToString { asString() }
+        } else originalString.toString()
+    }
 
     override fun hasChildren(): Boolean = children.isNotEmpty()
 
@@ -27,9 +34,19 @@ internal sealed class JsonLiteralImpl(
 
     abstract fun getName(): String
 
+    /**
+     * ABNFルール(RFC5234)に則った、%表記での文字列を表す。
+     *
+     * 他のJSONリテラルの子として使用するが、そのリテラルが文字列のみなら使用しない。
+     */
+    internal class ABNFString(originalString: String)
+        : JsonLiteralImpl(emptyList(), originalString) {
+        override fun getName(): String = "ABNFString"
+    }
+
     internal class WS private constructor(
         originalString: String
-    ) : JsonLiteralImpl(originalString, emptyList()) {
+    ) : JsonLiteralImpl(emptyList(), originalString) {
         override fun getName(): String = "WS"
 
         companion object Factory {
