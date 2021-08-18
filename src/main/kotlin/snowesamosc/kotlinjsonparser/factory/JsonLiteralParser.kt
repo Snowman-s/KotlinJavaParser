0b88d9cd3a6f71e0b72d47eb755deec964eefa5f
Snowman-s <snowman.snowsnow@gmail.com>
2021-08-18 13:13:17 +0900

    ドキュメントの修正

--- src/main/kotlin/snowesamosc/kotlinjsonparser/factory/JsonLiteralParser.kt
+++ src/main/kotlin/snowesamosc/kotlinjsonparser/factory/JsonLiteralParser.kt
@@ -4,6 +4,20 @@ import snowesamosc.kotlinjsonparser.node.JsonNode
 import snowesamosc.kotlinjsonparser.node.MissingNode
 
 class JsonLiteralParser {
+    /**
+     * Jsonテキストを解釈します。
+     *
+     * 与えられた文字列をJsonテキスト形式として解釈し、各々のValueをJsonNodeとして返します。
+     *
+     * この実装は、RFC8259によります。
+     *
+     * @see JsonNode
+     *
+     * @param str Json形式の文字列
+     * @return 最も上層のvalueを表すJsonNode
+     *
+     * @throws IllegalArgumentException 文字列が解釈出来なかった場合。
+     * */
     fun parse(str: String): JsonNode {
         val result = JsonLiteralImpl.JsonText.greedyCreate(str)
 
